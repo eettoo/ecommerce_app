5cd03e1aae5760d50f03912027f82a8f654bf59f
Haris <muhamad_haris@hotmail.com>
2017-10-06 14:36:12 +0800

    Add carrierwave gem and method if user log in, they will have a basket

--- app/controllers/shoppers/baskets_controller.rb
+++ app/controllers/shoppers/baskets_controller.rb
@@ -1,5 +1,7 @@
 class Shoppers::BasketsController < ApplicationController
 
+	before_action :create_current_basket
+
 	def show
 		@basket = current_user.baskets.first
 		@items_baskets = ItemsBasket.all
@@ -14,6 +16,17 @@ class Shoppers::BasketsController < ApplicationController
 	def destroy
 		@basket = Basket.find(params[:id])
 		@basket.destroy
+	end
+
+	private
+
+	def create_current_basket
+		if current_user.baskets.exists?
+		else
+			@basket = Basket.new
+			@basket.user = current_user
+			@basket.save
+		end
 	end
 
 end
