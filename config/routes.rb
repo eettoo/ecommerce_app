b08ea54b8165e888b2ac99d286ee0d24dce7ca5b
Haris <muhamad_haris@hotmail.com>
2017-10-09 16:39:41 +0800

    Edit links with navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: "registrations" }
 
 	# namespace :admin do
 	# 	resources :orders
@@ -14,14 +14,6 @@ Rails.application.routes.draw do
   get '/women' => 'pages#women'
 
   get '/men' => 'pages#men'
-
-
-
-  get '/shopping-cart' =>'pages#shopping-cart'
-
-
-
-
   
   namespace :shoppers do
 	  resources :products, only: [:index, :show]
@@ -35,9 +27,6 @@ Rails.application.routes.draw do
 	  	resources :items_paids
   	end
 	end
-
-
-
 
   root to: "pages#index"
 end
